5a39883c63c7bff6710ccb0bde054f8dceae1f5c
Mike Bild <mike@mikebild.com>
2022-08-23 08:22:10 +0200

    fix +layout bug

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,22 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 export default {
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+
+		// <workaround for https://github.com/sveltejs/kit/issues/5843>
+		{
+			config(config) {
+				if (config.build.rollupOptions.output) {
+					const original = config.build.rollupOptions.output.assetFileNames;
+					config.build.rollupOptions.output.assetFileNames = (assetInfo) => {
+						const match = assetInfo.name.match(/\/\+(.*)\.css$/);
+						return match ? original.replace('[name]', match[1]) : original;
+					};
+				}
+				return config;
+			}
+		}
+		// </workaround>
+	]
 };
